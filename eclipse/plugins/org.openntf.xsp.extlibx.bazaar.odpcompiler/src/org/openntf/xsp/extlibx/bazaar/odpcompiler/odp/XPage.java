21f709162b38e96d29fe4c53a3580398a354735b
Jesse Gallagher <jesse@secondfoundation.org>
2018-03-07 18:58:18 -0500

    Fix class name derivation for XSP artifacts

--- eclipse/plugins/org.openntf.xsp.extlibx.bazaar.odpcompiler/src/org/openntf/xsp/extlibx/bazaar/odpcompiler/odp/XPage.java
+++ eclipse/plugins/org.openntf.xsp.extlibx.bazaar.odpcompiler/src/org/openntf/xsp/extlibx/bazaar/odpcompiler/odp/XPage.java
@@ -24,7 +24,18 @@ public class XPage extends AbstractSourceDesignElement {
 	public String getJavaClassSimpleName() {
 		String pageName = getPageBaseName();
 		String capitalized = pageName.substring(0, 1).toUpperCase() + pageName.substring(1);
-		return capitalized;
+		
+		StringBuilder result = new StringBuilder();
+		for(int i = 0; i < capitalized.length(); i++) {
+			char c = capitalized.charAt(i);
+			if(!(Character.isAlphabetic(c) || Character.isDigit(c))) {
+				result.append('_');
+				result.append(String.format("%04x", (int)c));
+			} else {
+				result.append(c);
+			}
+		}
+		return result.toString();
 	}
 	
 	public String getPageName() {
