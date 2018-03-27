29440f0bb5b070c3cd749d6cabc00d7407d85d81
Jesse Gallagher <jesse@secondfoundation.org>
2018-03-27 16:50:53 -0400

    Add Maven plugin

---
+++ odp-compiler-maven-plugin/src/main/java/org/openntf/maven/odpcompiler/CompileODPMojo.java
@@ -0,0 +1,221 @@
+package org.openntf.maven.odpcompiler;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.DefaultMavenProjectHelper;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.codehaus.plexus.util.IOUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Goal which touches a timestamp file.
+ */
+@Mojo(name="compile-odp", defaultPhase=LifecyclePhase.COMPILE)
+public class CompileODPMojo extends AbstractMojo {
+	
+	@Parameter(defaultValue="${project}", readonly=true)
+	private MavenProject project;
+	
+	/**
+	 * Location of the generated NSF.
+	 */
+	@Parameter(defaultValue="${project.build.directory}", property="outputDir", required=true)
+	private File outputDirectory;
+	/**
+	 * File name of the generated NSF.
+	 */
+	@Parameter(defaultValue="output.nsf", required=true)
+	private String outputFileName;
+	/**
+	 * Location of the ODP directory.
+	 */
+	@Parameter(defaultValue="odp", required=true)
+	private File odpDirectory;
+	/**
+	 * The base URL of the ODP compiler server, e.g. "http://my.server".
+	 */
+	@Parameter(property="odpcompiler.compilerServer", required=true)
+	private URL compilerServer;
+	/**
+	 * An update site whose contents to use when building the ODP.
+	 */
+	@Parameter(required=false)
+	private File updateSite;
+	
+	private Log log;
+
+	public void execute() throws MojoExecutionException {
+		log = getLog();
+		
+		Path outputDirectory = Objects.requireNonNull(this.outputDirectory, "outputDirectory cannot be null").toPath();
+		
+		Path odpDirectory = Objects.requireNonNull(this.odpDirectory, "odpDirectory cannot be null").toPath();
+		if(!Files.exists(odpDirectory)) {
+			throw new IllegalArgumentException("Specified ODP directory does not exist: " + odpDirectory.toAbsolutePath());
+		}
+		if(!Files.isDirectory(odpDirectory)) {
+			throw new IllegalArgumentException("Specified ODP path is not a directory: " + odpDirectory.toAbsolutePath());
+		}
+		Path updateSite = this.updateSite == null ? null : this.updateSite.toPath();
+		if(updateSite != null) {
+			if(!Files.exists(updateSite)) {
+				throw new IllegalArgumentException("Specified Update Site directory does not exist: " + updateSite.toAbsolutePath());
+			}
+			if(!Files.isDirectory(updateSite)) {
+				throw new IllegalArgumentException("Specified Update Site path is not a directory: " + updateSite.toAbsolutePath());
+			}
+		}
+		String outputFileName = Objects.requireNonNull(this.outputFileName, "outputFileName cannot be null");
+		if(outputFileName.isEmpty()) {
+			throw new IllegalArgumentException("outputFileName cannot be empty");
+		}
+		
+		try {
+			if(!Files.exists(outputDirectory)) {
+				Files.createDirectories(outputDirectory);
+			}
+			
+			Path odpZip = zipDirectory(odpDirectory);
+			Path updateSiteZip = null;
+			if(updateSite != null) {
+				updateSiteZip = zipDirectory(updateSite);
+			}
+			
+			Path packageZip = createPackage(odpZip, updateSiteZip);
+			Path result = compileOdp(packageZip);
+			
+			Path outputFile = outputDirectory.resolve(outputFileName);
+			Files.move(result, outputFile, StandardCopyOption.REPLACE_EXISTING);
+			if(log.isInfoEnabled()) {
+				log.info("Generated NSF: " + outputFile);
+			}
+			
+			MavenProjectHelper helper = new DefaultMavenProjectHelper();
+			MavenProject project = Objects.requireNonNull(this.project, "Maven project cannot be null");
+			helper.attachArtifact(project, outputFile.toFile(), "nsf");
+			
+		} catch(Throwable t) {
+			throw new MojoExecutionException("Exception while compiling the NSF", t);
+		}
+	}
+	
+	private Path createPackage(Path odpZip, Path updateSiteZip) throws IOException {
+		if(log.isDebugEnabled()) {
+			log.debug("Creating package from odpZip=" + odpZip + ", updateSiteZip=" + updateSiteZip);
+		}
+		
+		Path packageZip = Files.createTempFile("odpcompiler-package", ".zip");
+		packageZip.toFile().deleteOnExit();
+		try(OutputStream fos = Files.newOutputStream(packageZip)) {
+			try(ZipOutputStream zos = new ZipOutputStream(fos)) {
+				ZipEntry entry = new ZipEntry("odp.zip");
+				zos.putNextEntry(entry);
+				Files.copy(odpZip, zos);
+				
+				if(updateSiteZip != null) {
+					entry = new ZipEntry("site.zip");
+					zos.putNextEntry(entry);
+					Files.copy(updateSiteZip, zos);
+				}
+			}
+		}
+		return packageZip;
+	}
+	
+	private Path compileOdp(Path packageZip) throws IOException, URISyntaxException {
+		if(log.isInfoEnabled()) {
+			log.info("Compiling ODP");
+		}
+		
+		try(CloseableHttpClient client = HttpClients.createDefault()) {
+			URI servlet = compilerServer.toURI().resolve("/odpcompiler");
+			if(log.isInfoEnabled()) {
+				log.info("Uploading ODP to " + servlet);
+			}
+			HttpPost post = new HttpPost(servlet);
+			post.addHeader("Content-Type", "application/zip");
+			
+			FileEntity fileEntity = new FileEntity(packageZip.toFile());
+			post.setEntity(fileEntity);
+			
+			HttpResponse res = client.execute(post);
+			int status = res.getStatusLine().getStatusCode();
+			HttpEntity responseEntity = res.getEntity();
+			if(log.isDebugEnabled()) {
+				log.debug("Received entity: " + responseEntity);
+			}
+			if(status < 200 || status >= 300) {
+				String errorBody;
+				try(ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+					try(InputStream is = responseEntity.getContent()) {
+						IOUtil.copy(is, baos);
+					}
+					errorBody = baos.toString();
+				}
+				System.err.println("Received error from server:");
+				System.err.println(errorBody);
+				throw new IOException("Received unexpected HTTP response: " + res.getStatusLine());
+			}
+			try(InputStream is = responseEntity.getContent()) {
+				Path result = Files.createTempFile("odpcompiler-output", ".nsf");
+				Files.copy(is, result, StandardCopyOption.REPLACE_EXISTING);
+				return result;
+			}
+		}
+	}
+	
+	private Path zipDirectory(Path path) throws IOException {
+		if(log.isDebugEnabled()) {
+			log.debug("Zipping path " + path.toString());
+		}
+		
+		Path result = Files.createTempFile("odpcompiler-dir", ".zip");
+		result.toFile().deleteOnExit();
+		
+		try(OutputStream fos = Files.newOutputStream(result)) {
+			try(ZipOutputStream zos = new ZipOutputStream(fos)) {
+				Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+					@Override
+					public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+						if(attrs.isRegularFile()) {
+							ZipEntry entry = new ZipEntry(path.relativize(file).toString());
+							zos.putNextEntry(entry);
+							Files.copy(file, zos);
+						}
+						return FileVisitResult.CONTINUE;
+					}
+				});
+			}
+		}
+		
+		return result;
+	}
+}
