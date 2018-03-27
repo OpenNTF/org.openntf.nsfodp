6837d80092d905a188e0bbe1dd0c7a057280818d
Jesse Gallagher <jesse@secondfoundation.org>
2018-03-27 16:53:40 -0400

    Add license header to Maven plugin

--- odp-compiler-maven-plugin/src/main/java/org/openntf/maven/odpcompiler/CompileODPMojo.java
+++ odp-compiler-maven-plugin/src/main/java/org/openntf/maven/odpcompiler/CompileODPMojo.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright © 2018 Jesse Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Includes code derived from the JNoSQL Diana Couchbase driver and Artemis
+ * extensions, copyright Otavio Santana and others and available from:
+ *
+ * https://github.com/eclipse/jnosql-diana-driver/tree/master/couchbase-driver
+ * https://github.com/eclipse/jnosql-artemis-extension/tree/master/couchbase-extension
+ */
 package org.openntf.maven.odpcompiler;
 
 import org.apache.http.HttpEntity;
