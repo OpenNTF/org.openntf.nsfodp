1c71cd6248268a58eb86bddb718220b980decc31
Jesse Gallagher <jesse@secondfoundation.org>
2018-03-29 15:48:11 -0400

    Add basic file associations for ODP content

---
+++ eclipse/plugins/org.openntf.xsp.extlibx.bazaar.odpcompiler.eclipse/src/org/openntf/xsp/extlibx/bazaar/odpcompiler/eclipse/content/JavaLibraryContentDescriber.java
@@ -0,0 +1,5 @@
+package org.openntf.xsp.extlibx.bazaar.odpcompiler.eclipse.content;
+
+public class JavaLibraryContentDescriber extends DXLContentDescriber {
+
+}
---
+++ eclipse/plugins/org.openntf.xsp.extlibx.bazaar.odpcompiler.eclipse/src/org/openntf/xsp/extlibx/bazaar/odpcompiler/eclipse/content/ThemeContentDescriber.java
@@ -0,0 +1,7 @@
+package org.openntf.xsp.extlibx.bazaar.odpcompiler.eclipse.content;
+
+import org.eclipse.core.runtime.content.XMLContentDescriber;
+
+public class ThemeContentDescriber extends XMLContentDescriber {
+
+}
