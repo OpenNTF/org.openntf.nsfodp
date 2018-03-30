dea60f204605148447d7fc39383fe0c26de2e927
Jesse Gallagher <jesse@secondfoundation.org>
2018-03-30 17:47:44 -0400

    Fix more package names

---
+++ eclipse/plugins/org.openntf.nsfodp.eclipse/src/org/openntf/nsfodp/compiler/eclipse/m2e/ODPProjectConfigurator.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright © 2018 Jesse Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openntf.nsfodp.compiler.eclipse.m2e;
+
+import org.apache.maven.project.MavenProject;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
+import org.eclipse.m2e.core.project.configurator.ILifecycleMappingConfiguration;
+import org.eclipse.m2e.core.project.configurator.MojoExecutionKey;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+import org.eclipse.m2e.jdt.IClasspathDescriptor;
+import org.eclipse.m2e.jdt.IJavaProjectConfigurator;
+import org.eclipse.pde.internal.core.util.CoreUtility;
+import org.openntf.nsfodp.compiler.eclipse.nature.OnDiskProjectNature;
+
+@SuppressWarnings("restriction")
+public class ODPProjectConfigurator extends AbstractProjectConfigurator implements IJavaProjectConfigurator {
+
+	@Override
+	public void configureClasspath(IMavenProjectFacade facade, IClasspathDescriptor classpath, IProgressMonitor monitor) throws CoreException {
+
+	}
+
+	@Override
+	public void configureRawClasspath(ProjectConfigurationRequest request, IClasspathDescriptor classpath, IProgressMonitor monitor) throws CoreException {
+
+	}
+
+	@Override
+	public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
+		IProject project = request.getProject();
+    	MavenProject mavenProject = request.getMavenProject();
+		ODPPDEUtil.INSTANCE.addPDENature(project, mavenProject, monitor);
+
+		if(!project.hasNature(OnDiskProjectNature.ID)) {
+			CoreUtility.addNatureToProject(project, OnDiskProjectNature.ID, null);
+		}
+	}
+
+	@Override
+	public boolean hasConfigurationChanged(IMavenProjectFacade newFacade,
+			ILifecycleMappingConfiguration oldProjectConfiguration, MojoExecutionKey key, IProgressMonitor monitor) {
+		return false;
+	}
+}
