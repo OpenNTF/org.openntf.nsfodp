6f2b0c644ce6db92de0a2db643cf9d03ccdc9f74
Jesse Gallagher <jesse@secondfoundation.org>
2018-04-04 13:55:34 -0400

    Clean up CLIApplication a bit and bind HTTP to localhost only

--- eclipse/plugins/org.openntf.nsfodp.cli/src/org/openntf/nsfodp/cli/CLIApplication.java
+++ eclipse/plugins/org.openntf.nsfodp.cli/src/org/openntf/nsfodp/cli/CLIApplication.java
@@ -1,26 +1,15 @@
 package org.openntf.nsfodp.cli;
 
-import java.io.Console;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.security.PrivilegedExceptionAction;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.eclipse.equinox.http.jetty.JettyConfigurator;
 import org.eclipse.equinox.http.jetty.JettyConstants;
-import org.openntf.nsfodp.compiler.ODPCompiler;
-import org.openntf.nsfodp.compiler.odp.OnDiskProject;
-import org.openntf.nsfodp.compiler.update.FilesystemUpdateSite;
-import org.openntf.nsfodp.compiler.update.UpdateSite;
 import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
+
+import lotus.domino.NotesThread;
 
 public class CLIApplication implements IApplication {
 	Class<?> configurator;
@@ -31,15 +20,14 @@ public class CLIApplication implements IApplication {
 		
 		System.out.println("Listening for compiler requests on port " + httpPort);
 		
-		Bundle notesJar = Platform.getBundle("com.ibm.notes.java.api");
-		System.out.println("notesJar: " + notesJar);
-		System.out.println("jar status: " + notesJar.getState());
-		System.out.println("Base: " + notesJar.loadClass("lotus.domino.Base"));
+		NotesThread.sinitThread();
 		
+		// Do this reflectively because otherwise there's a ClassNotFoundException for some reason
 		Bundle jetty = Platform.getBundle("org.eclipse.equinox.http.jetty");
 		configurator = jetty.loadClass("org.eclipse.equinox.http.jetty.JettyConfigurator");
 		Dictionary<String, String> jettyConfig = new Hashtable<>();
 		jettyConfig.put(JettyConstants.HTTP_PORT, String.valueOf(httpPort));
+		jettyConfig.put(JettyConstants.HTTP_HOST, "127.0.0.1");
 		configurator.getMethod("startServer", String.class, Dictionary.class).invoke(null, getClass().getPackage().getName(), jettyConfig);
 		
 		System.in.read();
@@ -51,6 +39,8 @@ public class CLIApplication implements IApplication {
 	public void stop() {
 		try {
 			configurator.getMethod("stopServer", String.class).invoke(null, getClass().getPackage().getName());
+			
+			NotesThread.stermThread();
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
