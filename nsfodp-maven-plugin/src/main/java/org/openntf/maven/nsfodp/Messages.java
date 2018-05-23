2e1fa9b11d440d828eaf817586a6f4cd724366b2
Jesse Gallagher <jesse@secondfoundation.org>
2018-05-23 18:14:32 -0400

    Fix a problem where Maven didn’t package the messages bundle properly

--- nsfodp-maven-plugin/src/main/java/org/openntf/maven/nsfodp/Messages.java
+++ nsfodp-maven-plugin/src/main/java/org/openntf/maven/nsfodp/Messages.java
@@ -5,7 +5,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "org.openntf.maven.nsfodp.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
