4b3832f87dc699783530971c968a6ee958b73d17
Jesse Gallagher <jesse@secondfoundation.org>
2019-02-22 12:29:25 -0500

    Update copyright year

--- eclipse/plugins/org.openntf.nsfodp.eclipse/src/org/openntf/nsfodp/eclipse/contentassist/proposals/CustomControlCompletionProposal.java
+++ eclipse/plugins/org.openntf.nsfodp.eclipse/src/org/openntf/nsfodp/eclipse/contentassist/proposals/CustomControlCompletionProposal.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2018 Jesse Gallagher
+ * Copyright © 2018-2019 Jesse Gallagher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.openntf.nsfodp.eclipse.contentassist.proposals;
 
 import org.eclipse.swt.graphics.Image;
