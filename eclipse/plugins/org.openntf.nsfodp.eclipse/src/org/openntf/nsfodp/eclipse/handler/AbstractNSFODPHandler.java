7f01d07960bc8aba07af3c9a0a86cc34d42870f8
Jesse Gallagher <jesse@secondfoundation.org>
2019-03-12 11:41:45 -0400

    Restrict Eclipse actions to applicable projects (Issue #96)

---
+++ eclipse/plugins/org.openntf.nsfodp.eclipse/src/org/openntf/nsfodp/eclipse/handler/AbstractNSFODPHandler.java
@@ -0,0 +1,60 @@
+package org.openntf.nsfodp.eclipse.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+import org.openntf.nsfodp.eclipse.nature.OnDiskProjectNature;
+
+public abstract class AbstractNSFODPHandler extends AbstractHandler {
+
+	protected static IProject getSelectedProject() {
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if (window != null) {
+			IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
+			Object firstElement = selection.getFirstElement();
+			if (firstElement instanceof IAdaptable) {
+				IProject project = (IProject) ((IAdaptable) firstElement).getAdapter(IProject.class);
+				return project;
+			}
+		}
+		return null;
+	}
+
+	protected static MessageConsole findConsole(String name) {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = plugin.getConsoleManager();
+		IConsole[] existing = conMan.getConsoles();
+		for (int i = 0; i < existing.length; i++) {
+			if (name.equals(existing[i].getName())) {
+				return (MessageConsole) existing[i];
+			}
+		}
+		// no console found, so create a new one
+		MessageConsole myConsole = new MessageConsole(name, null);
+		conMan.addConsoles(new IConsole[] { myConsole });
+		return myConsole;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		IProject project = getSelectedProject();
+		if(project != null && project.isAccessible()) {
+			try {
+				return project.hasNature(OnDiskProjectNature.ID);
+			} catch (CoreException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return false;
+	}
+
+}
--- eclipse/plugins/org.openntf.nsfodp.eclipse/src/org/openntf/nsfodp/eclipse/handler/CompileODPHandler.java
+++ eclipse/plugins/org.openntf.nsfodp.eclipse/src/org/openntf/nsfodp/eclipse/handler/CompileODPHandler.java
@@ -17,22 +17,14 @@ package org.openntf.nsfodp.eclipse.handler;
 
 import java.io.PrintWriter;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.console.ConsolePlugin;
-import org.eclipse.ui.console.IConsole;
-import org.eclipse.ui.console.IConsoleManager;
 import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.console.MessageConsoleStream;
 import org.openntf.nsfodp.eclipse.job.CompileODPJob;
 
-public class CompileODPHandler extends AbstractHandler {
+public class CompileODPHandler extends AbstractNSFODPHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {
@@ -50,34 +42,5 @@ public class CompileODPHandler extends AbstractHandler {
 			}
 		}
 		return null;
-	}
-	
-	private static IProject getSelectedProject() {
-		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-		if (window != null) {
-			IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
-			Object firstElement = selection.getFirstElement();
-			if (firstElement instanceof IAdaptable) {
-				IProject project = (IProject) ((IAdaptable) firstElement).getAdapter(IProject.class);
-				return project;
-			}
-		}
-		return null;
-	}
-
-	// h/t https://wiki.eclipse.org/FAQ_How_do_I_write_to_the_console_from_a_plug-in%3F
-	private static MessageConsole findConsole(String name) {
-		ConsolePlugin plugin = ConsolePlugin.getDefault();
-		IConsoleManager conMan = plugin.getConsoleManager();
-		IConsole[] existing = conMan.getConsoles();
-		for (int i = 0; i < existing.length; i++) {
-			if (name.equals(existing[i].getName())) {
-				return (MessageConsole) existing[i];
-			}
-		}
-		// no console found, so create a new one
-		MessageConsole myConsole = new MessageConsole(name, null);
-		conMan.addConsoles(new IConsole[] { myConsole });
-		return myConsole;
 	}
 }
