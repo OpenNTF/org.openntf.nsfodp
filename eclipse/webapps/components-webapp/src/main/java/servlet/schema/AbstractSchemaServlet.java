6e9ff21ecd23974e07457daeb5eff0286d7cb7b2
Jesse Gallagher <jesse@secondfoundation.org>
2020-01-01 09:41:50 -0500

    Rework generated XML Schemas to avoid subclassing and get a normal XPage validating

--- eclipse/webapps/components-webapp/src/main/java/servlet/schema/AbstractSchemaServlet.java
+++ eclipse/webapps/components-webapp/src/main/java/servlet/schema/AbstractSchemaServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalTime;
 import java.time.temporal.TemporalAccessor;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
@@ -15,7 +14,6 @@ import javax.faces.component.UIComponent;
 import javax.faces.el.MethodBinding;
 import javax.faces.el.ValueBinding;
 import javax.faces.event.FacesListener;
-import javax.faces.event.ValueChangeListener;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,21 +27,16 @@ import com.ibm.commons.util.StringUtil;
 import com.ibm.commons.xml.DOMUtil;
 import com.ibm.commons.xml.Format;
 import com.ibm.commons.xml.XMLException;
-import com.ibm.xsp.component.UIDataPanelBase;
-import com.ibm.xsp.component.UIFormEx;
-import com.ibm.xsp.component.UIViewRootEx;
-import com.ibm.xsp.component.xp.XspPager;
 import com.ibm.xsp.library.LibraryServiceLoader;
 import com.ibm.xsp.library.LibraryWrapper;
 import com.ibm.xsp.library.XspLibrary;
 import com.ibm.xsp.registry.AbstractContainerProperty;
-import com.ibm.xsp.registry.FacesComponentDefinition;
 import com.ibm.xsp.registry.FacesDefinition;
 import com.ibm.xsp.registry.FacesProperty;
+import com.ibm.xsp.registry.FacesSimpleProperty;
 import com.ibm.xsp.registry.SharableRegistryImpl;
 import com.ibm.xsp.registry.config.SimpleRegistryProvider;
 import com.ibm.xsp.registry.config.XspRegistryProvider;
-import com.ibm.xsp.resource.Resource;
 
 public class AbstractSchemaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -54,15 +47,6 @@ public class AbstractSchemaServlet extends HttpServlet {
 		extMap.put("http://www.ibm.com/xsp/core", "xp");
 		extMap.put("http://www.ibm.com/xsp/coreex", "xe");
 	}
-	// Classes that show up as parents but are squelched in the library definitions
-	private static final List<Class<?>> fauxComponents = Arrays.asList(
-		UIFormEx.class,
-		XspPager.class
-	);
-	// Classes that show up as parents but have no library definition at all
-	private static final List<Class<?>> noComponents = Arrays.asList(
-		UIViewRootEx.class
-	);
 	
 	private final String namespace;
 	private final String[] imports;
@@ -112,14 +96,9 @@ public class AbstractSchemaServlet extends HttpServlet {
 			facesRegistry.refreshReferences();
 			
 			@SuppressWarnings("unchecked")
-			List<FacesDefinition> defs = (List<FacesDefinition>)(List<?>)facesRegistry.findComponentDefs();
+			List<FacesDefinition> defs = (List<FacesDefinition>)(List<?>)facesRegistry.findDefs();
 			defs.stream()
-				.filter(def -> StringUtil.equals(namespace, def.getNamespaceUri()))
-				.forEach(def -> outComponentDefinition(def, schema, facesRegistry, false));
-			
-			@SuppressWarnings("unchecked")
-			List<FacesDefinition> complexDefs = (List<FacesDefinition>)(List<?>)facesRegistry.findComplexDefs();
-			complexDefs.stream()
+				.filter(FacesDefinition::isTag)
 				.filter(def -> StringUtil.equals(namespace, def.getNamespaceUri()))
 				.forEach(def -> outComponentDefinition(def, schema, facesRegistry, false));
 			
@@ -184,13 +163,25 @@ public class AbstractSchemaServlet extends HttpServlet {
 		
 		// Binding-only types
 		{
+			// TODO allow text before and/or after this, but require a binding.
+			//    May have to be done programmatically
 			Element simpleType = DOMUtil.createElement(doc, schema, "xs:simpleType");
 			simpleType.setAttribute("name", "attrBinding");
 			Element restriction = DOMUtil.createElement(doc, simpleType, "xs:restriction");
 			restriction.setAttribute("base", "xs:string");
 			Element pattern = DOMUtil.createElement(doc, restriction, "xs:pattern");
-			// TODO figure out if this can enforce a date format
 			pattern.setAttribute("value", "[#$]\\{[.\\s\\S]*\\}");
+		}
+		// String or load-time binding
+		{
+			// TODO this may have to be enforced programmatically, so this doesn't work for now
+			Element simpleType = DOMUtil.createElement(doc, schema, "xs:simpleType");
+			simpleType.setAttribute("name", "attrLoadBinding");
+			Element restriction = DOMUtil.createElement(doc, simpleType, "xs:restriction");
+			restriction.setAttribute("base", "xs:string");
+			Element pattern = DOMUtil.createElement(doc, restriction, "xs:pattern");
+			// TODO allow text before and/or after this, but require a binding
+			pattern.setAttribute("value", "$\\{[.\\s\\S]*\\}");
 		}
 	}
 	
@@ -219,48 +210,18 @@ public class AbstractSchemaServlet extends HttpServlet {
 		}
 		
 		
-		// Find if it's the child of a complex type
-		FacesDefinition maybeParent = def.getParent();
-		Element attrRoot;
-		if(maybeParent != null) {
-			if(noComponents.contains(maybeParent.getJavaClass())) {
-				maybeParent = maybeParent.getParent();
-			}
-			if(fauxComponents.contains(maybeParent.getJavaClass())) {
-				outComponentDefinition((FacesComponentDefinition)maybeParent, schema, registry, true);
-			}
-			
-			Element complexContent = DOMUtil.createElement(doc, complexType, "xs:complexContent");
-			Element extension = DOMUtil.createElement(doc, complexContent, "xs:extension");
-			String base;
-			if(maybeParent.isTag() && StringUtil.isNotEmpty(maybeParent.getTagName()) && !fauxComponents.contains(maybeParent.getJavaClass())) {
-				base = extMap.get(maybeParent.getNamespaceUri()) + ":" + maybeParent.getTagName();
-			} else {
-				base = extMap.get(maybeParent.getNamespaceUri()) + ":" + maybeParent.getJavaClass().getName();
-			}
-			extension.setAttribute("base", base);
-			attrRoot = extension;
-		} else {
-			attrRoot = complexType;
-			
-			if(def.getDefinedPropertyNames().isEmpty()) {
-				// Add an xs:anyAttribute here to make this extensible down the line
-				DOMUtil.createElement(complexType.getOwnerDocument(), complexType, "xs:anyAttribute");
-			}
+		Element all = (Element)complexType.getFirstChild();
+		if(all == null || !all.getTagName().equals("xs:choice")) {
+			all = DOMUtil.createElement(doc, complexType, "xs:choice");
+			all.setAttribute("minOccurs", "0");
+			all.setAttribute("maxOccurs", "unbounded");
 		}
 		
-		outProperties(def, attrRoot, registry);
+		outProperties(def, complexType, registry);
 
 		if(UIComponent.class.isAssignableFrom(def.getJavaClass())) {
 			// Declare that it allows any children
-			// TODO see if this can restrict to text + UIComponents. This is tough because HTML tags are allowed
-			Element all = (Element)attrRoot.getFirstChild();
-			if(all == null || !all.getTagName().equals("xs:choice")) {
-				all = DOMUtil.createElement(doc, attrRoot, "xs:choice");
-				all.setAttribute("minOccurs", "0");
-				all.setAttribute("maxOccurs", "unbounded");
-			}
-			DOMUtil.createElement(doc, all, "xs:any");
+			DOMUtil.createElement(doc, all, "xs:any").setAttribute("processContents", "lax");
 		}
 		
 		if(def.isTag() && !noTag) {
@@ -272,39 +233,32 @@ public class AbstractSchemaServlet extends HttpServlet {
 
 //			Element annotation = DOMUtil.createElement(doc, element, "xs:annotation");
 			
-		} else {
-			if(!(
-				MethodBinding.class.isAssignableFrom(def.getJavaClass())
-				|| ValueBinding.class.isAssignableFrom(def.getJavaClass())
-			)) {
-				// Output an abstract representation
-//				complexType.setAttribute("abstract", "true");
-			}
 		}
+		
 	}
 	
 	private void outProperties(FacesDefinition def, Element element, SharableRegistryImpl registry) {
 		Collection<String> names = def.getPropertyNames();
 		for(String propName : names) {
 			FacesProperty prop = def.getProperty(propName);
-			
-			// Make sure it's not defined in a compatible parent
-			FacesDefinition maybeParent = def.getParent();
-			boolean foundMatch = false;
-			while(maybeParent != null && StringUtil.equals(def.getNamespaceUri(), maybeParent.getNamespaceUri())) {
-				// <xp:view/> is defined multiple times, so handle it specially
-				if(!UIViewRootEx.class.equals(maybeParent.getJavaClass()) && maybeParent.isDefinedProperty(propName)) {
-					foundMatch = true;
-					break;
-				}
-				maybeParent = maybeParent.getParent();
-			}
-			if(foundMatch) {
-				continue;
-			}
-
 			outProperty(def, prop, element, registry);
 		}
+		
+		// Everything allows id
+		if(!names.contains("id")) {
+			Element all = (Element)element.getFirstChild();
+			Element thisId = DOMUtil.createElement(element.getOwnerDocument(), all, "xs:element");
+			thisId.setAttribute("name", "this.id");
+			thisId.setAttribute("type", extMap.get(this.namespace) + ":attrLoadBinding");
+			Element attrId = DOMUtil.createElement(element.getOwnerDocument(), element, "xs:attribute");
+			attrId.setAttribute("name", "id");
+			attrId.setAttribute("type", "xs:ID");
+		}
+		
+//		if(names.isEmpty()) {
+//			// Add an xs:anyAttribute here to make this extensible down the line
+//			DOMUtil.createElement(element.getOwnerDocument(), element, "xs:anyAttribute");
+//		}
 	}
 	
 	private void outProperty(FacesDefinition def, FacesProperty prop, Element element, SharableRegistryImpl registry) {
@@ -313,7 +267,9 @@ public class AbstractSchemaServlet extends HttpServlet {
 		if(prop instanceof AbstractContainerProperty) {
 			clazz = ((AbstractContainerProperty)prop).getItemProperty().getJavaClass();
 		}
-		if(byte.class.equals(clazz) || short.class.equals(clazz) || int.class.equals(clazz) || long.class.equals(clazz)
+		if(isBindingType(prop)) {
+			outAttribute(def, prop, element, extMap.get(this.namespace) + ":attrBinding", false);
+		} else if(byte.class.equals(clazz) || short.class.equals(clazz) || int.class.equals(clazz) || long.class.equals(clazz)
 			|| Byte.class.equals(clazz) || Short.class.equals(clazz) || Integer.class.equals(clazz) || Long.class.equals(clazz)) {
 			outAttribute(def, prop, element, extMap.get(namespace) + ":attrInteger", false);
 		} else if(boolean.class.equals(clazz) || Boolean.class.equals(clazz)) {
@@ -324,6 +280,8 @@ public class AbstractSchemaServlet extends HttpServlet {
 			outAttribute(def, prop, element, extMap.get(namespace) + ":attrTime", false);
 		} else if(Date.class.equals(clazz) || TemporalAccessor.class.isAssignableFrom(clazz)) {
 			outAttribute(def, prop, element, extMap.get(namespace) + ":attrDate", false);
+		} else if("id".equals(prop.getName())) {
+			outAttribute(def, prop, element, "xs:ID", false);
 		} else if(CharSequence.class.isAssignableFrom(clazz)) {
 			outAttribute(def, prop, element, "xs:string", false);
 		} else if(Object.class.equals(clazz) || FacesListener.class.isAssignableFrom(clazz)) {
@@ -349,45 +307,27 @@ public class AbstractSchemaServlet extends HttpServlet {
 		Document doc = element.getOwnerDocument();
 		
 		Element all = (Element)element.getFirstChild();
-		if(all == null || !all.getTagName().equals("xs:choice")) {
-			all = DOMUtil.createElement(doc, element, "xs:choice");
-			all.setAttribute("minOccurs", "0");
-			all.setAttribute("maxOccurs", "unbounded");
-		}
 		
 		// Create a this.child child
 		Element thisElement = DOMUtil.createElement(doc, all, "xs:element");
 		thisElement.setAttribute("name", "this." + prop.getName());
-		if(complex && !isBindingType(prop)) {
-			// TODO see if there's a way to require elements of a given subclass
-			/*
-			 * <xs:complexType>
-          		<xs:sequence>
-          			<xs:any></xs:any>
-          		</xs:sequence>
-          	</xs:complexType>
-			 */
+		if(complex || isBindingType(prop)) {
 			Element complexType = DOMUtil.createElement(doc, thisElement, "xs:complexType");
+			if(isBindingType(prop)) {
+				// Bindings are a mess
+				complexType.setAttribute("mixed", "true");
+			}
 			Element sequence = DOMUtil.createElement(doc, complexType, "xs:sequence");
 			if(prop instanceof AbstractContainerProperty) {
 				// Then mark its upper bound as unlimited
 				sequence.setAttribute("maxOccurs", "unbounded");
 			}
+			sequence.setAttribute("minOccurs", "0");
 			DOMUtil.createElement(doc, sequence, "xs:any").setAttribute("maxOccurs", "unbounded");
 		} else {
 			thisElement.setAttribute("type", type);
 		}
 		
-		// Only output the attribute if no parent also names it
-		FacesDefinition maybeParent = def.getParent();
-		while(maybeParent != null) {
-			if(maybeParent.isDefinedProperty(prop.getName())) {
-				// Then exit the whole thing
-				return;
-			}
-			
-			maybeParent = maybeParent.getParent();
-		}
 		Element attribute = DOMUtil.createElement(doc, element, "xs:attribute");
 		attribute.setAttribute("name", prop.getName());
 		if(complex) {
@@ -414,8 +354,14 @@ public class AbstractSchemaServlet extends HttpServlet {
 	}
 	
 	private boolean isBindingType(FacesProperty prop) {
-		return
-			MethodBinding.class.isAssignableFrom(prop.getJavaClass())
-			|| ValueBinding.class.isAssignableFrom(prop.getJavaClass());
+		if(MethodBinding.class.isAssignableFrom(prop.getJavaClass())
+			|| ValueBinding.class.isAssignableFrom(prop.getJavaClass())) {
+			return true;
+		}
+		if(prop instanceof FacesSimpleProperty) {
+			FacesSimpleProperty simpleProp = (FacesSimpleProperty)prop;
+			return !simpleProp.isAllowNonBinding();
+		}
+		return false;
 	}
 }
