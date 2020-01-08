3012c7e26eb2bb484d310f5aeda1d3c098f42812
Jesse Gallagher <jesse@secondfoundation.org>
2020-01-08 14:06:34 -0500

    Remove unused imports

--- nsfodp/bundles/org.openntf.nsfodp.commons/src/org/openntf/nsfodp/commons/io/SwiperOutputStream.java
+++ nsfodp/bundles/org.openntf.nsfodp.commons/src/org/openntf/nsfodp/commons/io/SwiperOutputStream.java
@@ -27,10 +27,8 @@ import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
