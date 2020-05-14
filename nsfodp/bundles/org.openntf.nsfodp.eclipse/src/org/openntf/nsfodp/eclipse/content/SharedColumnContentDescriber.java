cfaf3a5d40a5f4c3c168d1d8606be934a4d5de1e
Jesse Gallagher <jesse@secondfoundation.org>
2020-05-14 08:47:51 -0400

    Add Shared Columns to the Eclipse plugins (Issue #216)

---
+++ nsfodp/bundles/org.openntf.nsfodp.eclipse/src/org/openntf/nsfodp/eclipse/content/SharedColumnContentDescriber.java
@@ -0,0 +1,10 @@
+package org.openntf.nsfodp.eclipse.content;
+
+/**
+ * 
+ * @author Jesse Gallagher
+ * @since 3.0.0
+ */
+public class SharedColumnContentDescriber extends DXLContentDescriber {
+
+}
