de77aef01a8a98201934c55f35c5ee8b5b3e3c9e
Jesse Gallagher <jesse@secondfoundation.org>
2021-05-19 10:23:04 -0400

    Add embedded JAR in example OSGi bundle

--- examples/xpages-library/nsfs/nsf-example/odp/Code/Java/org/openntf/nsfodp/example/nsf/ExampleClass.java
+++ examples/xpages-library/nsfs/nsf-example/odp/Code/Java/org/openntf/nsfodp/example/nsf/ExampleClass.java
@@ -1,6 +1,8 @@
-package org.openntf.nsfodp.example;
+package org.openntf.nsfodp.example.nsf;
 
 import org.apache.commons.lang3.SystemUtils;
+
+import com.ibm.jscript.InterpretException;
 
 /**
  * This is an example class.
@@ -17,6 +19,8 @@ public class ExampleClass {
 	private boolean is8 = SystemUtils.IS_JAVA_1_8;
 	// Class inside the plugin from the update site
 	private org.openntf.nsfodp.example.Activator pluginActivator = org.openntf.nsfodp.example.Activator.getDefault();
+	// Class inside a JAR embedded in the plugin
+	private com.example.bundleembed.BundleEmbeddedClass embedded = new com.example.bundleembed.BundleEmbeddedClass();
 	
 	public String getFoo() {
 		return foo;
@@ -24,5 +28,11 @@ public class ExampleClass {
 	
 	public void setFoo(String foo) {
 		this.foo = foo;
+	}
+	
+	// Handle a case where InterpretException has two versions on a Domino server's classpath
+	public void usingInterpretException() {
+		InterpretException e = new InterpretException(new Throwable("text"));
+		e.setExpressionText("hello");
 	}
 }
