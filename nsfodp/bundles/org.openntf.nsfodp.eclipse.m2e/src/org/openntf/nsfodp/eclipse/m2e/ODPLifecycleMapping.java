425674c5578b36aab47a6ca7a1e691b9c474325d
Jesse Gallagher <jesse@secondfoundation.org>
2023-04-20 15:37:37 -0400

    Adapt ODPProjectConfigurator and ODPLifecycleMapping for different m2e versions

--- nsfodp/bundles/org.openntf.nsfodp.eclipse.m2e/src/org/openntf/nsfodp/eclipse/m2e/ODPLifecycleMapping.java
+++ nsfodp/bundles/org.openntf.nsfodp.eclipse.m2e/src/org/openntf/nsfodp/eclipse/m2e/ODPLifecycleMapping.java
@@ -44,8 +44,10 @@ public class ODPLifecycleMapping extends AbstractCustomizableLifecycleMapping im
     private static final AbstractMavenDependencyResolver NOOP_DEPENDENCY_RESOLVER =
             new AbstractMavenDependencyResolver()
             {
-                @Override
-                public void resolveProjectDependencies( IMavenProjectFacade facade, MavenExecutionRequest mavenRequest,
+    	// No longer present in newer versions
+//                @Override
+                @SuppressWarnings("unused")
+				public void resolveProjectDependencies( IMavenProjectFacade facade, MavenExecutionRequest mavenRequest,
                                                         Set<Capability> capabilities, Set<RequiredCapability> requirements,
                                                         IProgressMonitor monitor )
                     throws CoreException
