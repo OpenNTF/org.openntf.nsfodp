6917b60ba789f29894429d2e46a0cee47506cdea
Jesse Gallagher <jesse@secondfoundation.org>
2026-03-25 10:33:10 -0400

    Remove unused imports

--- nsfodp/maven/nsfodp-maven-plugin/src/main/java/org/openntf/maven/nsfodp/container/NSFODPContainer.java
+++ nsfodp/maven/nsfodp-maven-plugin/src/main/java/org/openntf/maven/nsfodp/container/NSFODPContainer.java
@@ -22,7 +22,6 @@ import java.io.UncheckedIOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.text.MessageFormat;
@@ -42,7 +41,6 @@ import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.images.builder.Transferable;
 
 import com.github.dockerjava.api.command.InspectContainerResponse;
-import com.ibm.commons.util.PathUtil;
 import com.ibm.commons.util.StringUtil;
 
 public class NSFODPContainer extends GenericContainer<NSFODPContainer> {
