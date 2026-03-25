107c3f8bac084fea331c52f33379e8dfe5f77154
Jesse Gallagher <jesse@secondfoundation.org>
2026-03-25 11:32:17 -0400

    Adjust property name for timeZone

--- nsfodp/maven/nsfodp-maven-plugin/src/main/java/org/openntf/maven/nsfodp/AbstractEquinoxMojo.java
+++ nsfodp/maven/nsfodp-maven-plugin/src/main/java/org/openntf/maven/nsfodp/AbstractEquinoxMojo.java
@@ -171,7 +171,7 @@ public abstract class AbstractEquinoxMojo extends AbstractMojo {
 	 * @see <a href="https://en.wikipedia.org/wiki/List_of_tz_database_time_zones">List of tz database time zones</a>
 	 * @since 4.1.0
 	 */
-	@Parameter(property="nsfodp.compiler.timeZone", required=false)
+	@Parameter(property="nsfodp.timeZone", required=false)
 	protected String timeZone;
 
 	protected boolean isRunLocally() {
